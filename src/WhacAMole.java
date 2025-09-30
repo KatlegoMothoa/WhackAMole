a58a9e918f11506333ca5c0b276396cad9a5f26b
KatlegoMothoa <kmothoa50@gmail.com>
2025-09-30 19:21:19 +0200

    working on the frame

--- src/WhacAMole.java
+++ src/WhacAMole.java
@@ -1,3 +1,32 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Random;
+import javax.swing.*;
+
 public class WhacAMole {
+    int boardWidth = 600;
+    int boardHeight = 650;
+
+    JFrame frame = new JFrame("KatKodes: Whack A Mole");
+    JLabel textLabel = new JLabel();
+    JPanel textPanel = new JPanel();
     
+    WhacAMole(){
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+
+        textLabel.setFont(new Font("Arial", Font.PLAIN, 50));
+        textLabel.setHorizontalAlignment(JLabel.CENTER);
+        textLabel.setText("Score: 0");
+        textLabel.setOpaque(true);
+
+        textPanel.setLayout(new BorderLayout());
+        textPanel.add(textLabel);
+        frame.add(textPanel, BorderLayout.NORTH);
+ 
+    }
 }
